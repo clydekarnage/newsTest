0aca9910b4ea78e35832cf73bf233ccbefd34636
clydekarnage <ibalezclyde@gmail.com>
2022-05-19 02:13:43 +0400

    installed and configured react-router

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-     
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={ <Home/> } />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
